287724ff227d79576d02497d448a20e4b2268dca
Viktor Kojouharov <developer@sugr.org>
2016-03-11 02:02:49 +0200

    affine transformations
    add percentage scale support

--- operation/transform/scale.go
+++ operation/transform/scale.go
@@ -3,6 +3,8 @@ package transform
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/urandom/drawgl"
 	"github.com/urandom/drawgl/interpolator"
@@ -18,20 +20,22 @@ type Scale struct {
 }
 
 type ScaleOptions struct {
-	Width, Height int
-	Interpolator  interpolator.Interpolator
-	Channel       drawgl.Channel
-	Mask          drawgl.Mask
-	Linear        bool
+	Width, Height               int
+	WidthPercent, HeightPercent float64
+	Interpolator                interpolator.Interpolator
+	Channel                     drawgl.Channel
+	Mask                        drawgl.Mask
+	Linear                      bool
 }
 
 type jsonScaleOptions struct {
 	ScaleOptions
 	InterpolatorType string `json:"Interpolator"`
+	Width, Height    string
 }
 
 func NewScaleLinker(opts ScaleOptions) (graph.Linker, error) {
-	if opts.Width <= 0 && opts.Height <= 0 {
+	if opts.Width <= 0 && opts.Height <= 0 && opts.WidthPercent <= 0 && opts.HeightPercent <= 0 {
 		return nil, fmt.Errorf("invalid width %f and height %f, at least one has to be positive", opts.Width, opts.Height)
 	}
 
@@ -72,10 +76,22 @@ func (n Scale) Process(wd graph.WalkData, buffers map[graph.ConnectorName]drawgl
 	b := src.Bounds()
 	dr := src.Bounds()
 
-	tW, tH := n.opts.Width, n.opts.Height
-	if tW <= 0 || tH <= 0 {
-		width, height := b.Max.X-b.Min.X, b.Max.Y-b.Min.Y
+	width, height := b.Dx(), b.Dy()
 
+	var tW, tH int
+	if n.opts.Width != 0 {
+		tW = n.opts.Width
+	} else if n.opts.WidthPercent != 0 {
+		tW = int(n.opts.WidthPercent * float64(width))
+	}
+
+	if n.opts.Height != 0 {
+		tH = n.opts.Height
+	} else if n.opts.HeightPercent != 0 {
+		tH = int(n.opts.HeightPercent * float64(height))
+	}
+
+	if tW <= 0 || tH <= 0 {
 		if tW <= 0 {
 			tW = tH * width / height
 		} else {
@@ -99,6 +115,31 @@ func init() {
 
 		if err := json.Unmarshal([]byte(opts), &o); err != nil {
 			return nil, fmt.Errorf("constructing Scale: %v", err)
+		}
+
+		dimens := [2]string{o.Width, o.Height}
+		for i := 0; i < 2; i++ {
+			if dimens[i] != "" {
+				if strings.HasSuffix(dimens[i], "%") {
+					dimens[i] = strings.TrimSpace(strings.TrimSuffix(dimens[i], "%"))
+					if pc, err := strconv.ParseFloat(dimens[i], 64); err == nil {
+						if i == 0 {
+							o.ScaleOptions.WidthPercent = pc / 100
+						} else {
+							o.ScaleOptions.HeightPercent = pc / 100
+						}
+					}
+				} else {
+					dimens[i] = strings.TrimSpace(strings.TrimSuffix(dimens[i], "px"))
+					if px, err := strconv.Atoi(dimens[i]); err == nil {
+						if i == 0 {
+							o.ScaleOptions.Width = px
+						} else {
+							o.ScaleOptions.Height = px
+						}
+					}
+				}
+			}
 		}
 
 		o.ScaleOptions.Interpolator = interpolator.Inst(o.InterpolatorType)
