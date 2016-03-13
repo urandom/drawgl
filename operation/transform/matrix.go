95143ec22e1c6cc3784374b45e2ccde9c4df7ce6
Viktor Kojouharov <developer@sugr.org>
2016-03-13 16:10:10 +0200

    affine transformation
    when calculating the inverse, actually copy the matrix data

--- operation/transform/matrix.go
+++ operation/transform/matrix.go
@@ -3,6 +3,7 @@ package transform
 import (
 	"image"
 	"image/draw"
+	"math"
 
 	"github.com/urandom/drawgl"
 	"github.com/urandom/drawgl/interpolator"
@@ -42,5 +43,48 @@ func affine(op transformOperation, src *drawgl.FloatImage, mask drawgl.Mask, cha
 		dst.UnsafeSetColor(pt.X, pt.Y, drawgl.MaskColor(orig, srcC, channel, f, draw.Over))
 	})
 
+	return
+}
+
+func affineTransformRect(m matrix.Matrix3, sr image.Rectangle) (dr image.Rectangle) {
+	ps := [...]image.Point{
+		{sr.Min.X, sr.Min.Y},
+		{sr.Max.X, sr.Min.Y},
+		{sr.Min.X, sr.Max.Y},
+		{sr.Max.X, sr.Max.Y},
+	}
+	for i, p := range ps {
+		sxf := float64(p.X)
+		syf := float64(p.Y)
+		dx := int(math.Floor(m[0][0]*sxf + m[0][1]*syf + m[0][2]))
+		dy := int(math.Floor(m[1][0]*sxf + m[1][1]*syf + m[1][2]))
+
+		// The +1 adjustments below are because an image.Rectangle is inclusive
+		// on the low end but exclusive on the high end.
+
+		if i == 0 {
+			dr = image.Rectangle{
+				Min: image.Point{dx + 0, dy + 0},
+				Max: image.Point{dx + 1, dy + 1},
+			}
+			continue
+		}
+
+		if dr.Min.X > dx {
+			dr.Min.X = dx
+		}
+		dx++
+		if dr.Max.X < dx {
+			dr.Max.X = dx
+		}
+
+		if dr.Min.Y > dy {
+			dr.Min.Y = dy
+		}
+		dy++
+		if dr.Max.Y < dy {
+			dr.Max.Y = dy
+		}
+	}
 	return
 }
--- operation/transform/matrix/matrix.go
+++ operation/transform/matrix/matrix.go
@@ -47,13 +47,13 @@ func (m Matrix3) IsTranslate() bool {
 
 // Determinant returns the determinant of the matrix
 func (m Matrix3) Determinant() float64 {
-	return m[0][0]*(m[1][1]*m[2][2]-m[1][2]*m[2][1]) +
-		m[0][1]*(m[1][2]*m[2][0]-m[1][0]*m[2][2]) +
+	return m[0][0]*(m[1][1]*m[2][2]-m[1][2]*m[2][1]) -
+		m[0][1]*(m[1][0]*m[2][2]-m[1][2]*m[2][0]) +
 		m[0][2]*(m[1][0]*m[2][1]-m[1][1]*m[2][0])
 }
 
 func (m *Matrix3) Invert() {
-	copy := m
+	copy := *m
 	coeff := 1 / m.Determinant()
 
 	m[0][0] = (copy[1][1]*copy[2][2] - copy[1][2]*copy[2][1]) * coeff
